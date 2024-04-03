ac577c0213e1ea310e3dedf988f57c8b22262de8
Kingdom <andersonixon12@gmail.com>
2024-04-03 17:24:15 +0300

    Structure

---
+++ src/main/java/org/ihfms/ihfms/IhfmsApplication.java
@@ -0,0 +1,13 @@
+package org.ihfms.ihfms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhfmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhfmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ihfms/ihfms/IhfmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ihfms.ihfms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhfmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
